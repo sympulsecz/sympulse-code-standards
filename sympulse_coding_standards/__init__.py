74be2a8b9e6558a0f1dc239ca4c83ba77cbb1b49
PetrCala <cala.p@seznam.cz>
2025-08-11 13:30:56 +0200

    refactor: define the pyproject deps with the poetry syntax

--- sympulse_coding_standards/__init__.py
+++ sympulse_coding_standards/__init__.py
@@ -4,13 +4,16 @@ __version__ = "0.2.0"
 __author__ = "Petr Čala"
 __email__ = "petr.cala@sympulse.cz"
 
-from .core import StandardsManager
-from .validators import ValidationResult
+from .core import StandardsManager, ValidationResult
+from .validators import PythonValidator, TypeScriptValidator, ValidationIssue
 from .generators import ProjectGenerator
 
 __all__ = [
     "StandardsManager",
     "ValidationResult",
     "ProjectGenerator",
+    "PythonValidator",
+    "TypeScriptValidator",
+    "ValidationIssue",
     "__version__",
 ]
